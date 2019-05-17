8208e98ef6a38b62e5a10d092bd65892286e7985
spras00 <50716392+spras00@users.noreply.github.com>
2019-05-17 23:16:36 +0700

    Revert "Update input_kriteria.php"
    This reverts commit 1717fa5714478dc98322711da315269de8d3dc9b.

--- application/views/input_kriteria.php
+++ application/views/input_kriteria.php
@@ -38,7 +38,7 @@
 			<div class="form-group"> 
     					<div class="col-sm-offset-4 col-sm-4">
       					<button type="submit" class="btn btn-success btn-block">Simpan</button>
-      					<button type="reset" onclick="location.href='<?php echo base_url();?>Kriteria'" class="btn btn-danger btn-block">Batal</button>
+      					<button type="reset" class="btn btn-danger btn-block">Batal</button>
   						</div>
   			</div>
 </div>
