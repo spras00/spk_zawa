bf518d6b62e75372531f10fba50f848bcc73ae5c
spras00 <50716392+spras00@users.noreply.github.com>
2019-05-18 01:41:37 +0700

    Update Model_Crips.php

--- application/models/Model_Crips.php
+++ application/models/Model_Crips.php
@@ -3,4 +3,59 @@
 
 Class Model_Crips extends CI_Model{
 	
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+		function tampil_data(){
+		$query = $this->db->get('_crips');
+		return $query->result_array();
+	}
+
+	function bacaid($id_cp = 0)
+	{
+		if($id_cp === 0)
+		{
+			$query =  $this->db->get('_crips');
+			return $query->result_array();
+		}
+
+		$query = $this->db->get_where('_crips', array('id_cp' => $id_cp));
+		return $query->row_array();
+	}
+
+	function combokriteria(){
+		$query = $this->db->get('_kriteria');
+		return $query->result();
+	}
+
+	function buat($id_cp = NULL)
+
+	{
+		
+		$data = array(
+			'id_cp' => $this->input->post('id_cp'),
+			'nm_cp'=> $this->input->post('nm_cp'),
+			'id_k' => $this->input->post('id_k'),
+			'skor'=> $this->input->post('skor')
+		);
+
+		if($id_cp == NULL)
+		{
+			return $this->db->insert('_crips', $data);	
+		}else
+		{
+			$this->db->where('id_cp', $id_cp);
+			return $this->db->update('_crips', $data);
+		}
+		
+	}
+
+	function hapus($id_cp)
+	{
+		$this->db->where('id_cp', $id_cp);
+		return $this->db->delete('_crips');
+	}
+
 }
