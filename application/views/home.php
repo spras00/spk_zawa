d2fab54f14d68e959165fedb29bb11b249986338
spras00 <50716392+spras00@users.noreply.github.com>
2019-05-17 23:16:20 +0700

    Revert "Update home.php"
    This reverts commit 34d5bcc9f9d40bcaf369d6f466c9d0aab20cdcf0.

--- application/views/home.php
+++
@@ -1,9 +0,0 @@
-<div class="alert alert-info" role="alert">
-	<p>SISTEM PENUNJANG KEPUTUSAN RUMAH TERBAIK</p>
-	<br>
-	<p>MENGGUNAKAN METODE SAW BERBASIS WEB</p>
-</div>
-<div class="row">
-  <div class="col-xs-6 col-md-3">
-  </div>
-  </div>
