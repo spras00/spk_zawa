9560d61394c7e3e537997a2b7e30d3f86d397fcf
spras00 <50716392+spras00@users.noreply.github.com>
2019-05-23 16:21:17 +0700

    Create view_crips2.php

---
+++ application/views/view_crips2.php
@@ -0,0 +1,46 @@
+</div>
+<table align="center" class="table table-condensed">
+	<thead>
+		<tr>
+			<th>
+				ID KRITERIA
+			</th>
+			<th>
+				NAMA KRITERIA
+			</th>
+			<th>
+				TAMBAH NILAI CRIPS
+			</th>
+			<th>
+				UBAH NILAI CRIPS
+			</th>
+		</tr>
+	</thead>
+		<?php
+		foreach ($kriteria as $kr) {?>
+			<tr>
+				<td>
+					<?php echo $kr['id_k']; ?>
+				</td>
+				<td>
+					<?php echo $kr['nm_k']; ?>
+				</td>
+				<td>
+					<a href="<?php echo base_url('crips/input/'.$kr['id_k']); ?>">
+						<button type="button" class="btn btn-default" action="">
+						<span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+						Tambah Data
+						</button>
+					</a>
+				</td>
+				<td>
+					<a href="<?php echo base_url('crips/index/'.$crp['id_cp']); ?>">
+						<button type="button" class="btn btn-default">
+  						<span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
+						</button>
+					</a>
+				</td>
+			</tr> <?php
+		} ?>
+</table>
+</div>
