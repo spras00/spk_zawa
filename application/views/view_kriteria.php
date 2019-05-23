66a035340d349df3790a445327592bfd03b1bcb1
spras00 <50716392+spras00@users.noreply.github.com>
2019-05-23 15:49:08 +0700

    Update view_kriteria.php

--- application/views/view_kriteria.php
+++ application/views/view_kriteria.php
@@ -1,3 +1,12 @@
+<div class="btn header-btn">
+	<a href="<?php echo site_url('kriteria/input'); ?>">
+		<button type="button" class="btn btn-default" action="">
+		<span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+		Tambah Data
+		</button>
+	</a>
+</div>	
+</div>
 <table align="center" class="table table-condensed">
 	<thead>
 		<tr>
