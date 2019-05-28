53a84769d98b39ad6c33b8b28096905c5bab77b2
spras00 <50716392+spras00@users.noreply.github.com>
2019-05-28 17:10:53 +0700

    Update edit_rumah.php

--- application/views/edit_rumah.php
+++ application/views/edit_rumah.php
@@ -8,6 +8,12 @@
 <?php echo form_open('rumah/edit/'.$rumah['id_a']);?>
 <div class="form-horizontal">
 			<div class="form-group">
+				<label for="id_alternatif" class="control-label col-sm-4">ID RUMAH</label>
+				<div class="col-xs-4">
+				<input type="text" class="form-control" name="id_a" value="<?php echo $rumah['id_a']?>" readonly>
+				</div>
+			</div>
+			<div class="form-group">
 				<label for="nm_alternatif" class="control-label col-sm-4">NAMA RUMAH</label>
 				<div class="col-xs-4">
 				<input type="text" class="form-control" name="nm_a" value="<?php echo $rumah['nm_a']?>">
@@ -41,13 +47,8 @@
 			<div class="form-group">
 				<label for="fasilitas" class="control-label col-sm-4">FASILITAS</label>
 				<div class="col-xs-4">
-				<select class="form-control" name="fasilitas">
-					<option value="">	Pilih Fasiltias	</option>
-					<option value="Un Furnish">Un Furnish</option>
-					<option value="Semi Furnish">Semi Furnish</option>
-					<option value="Full Furnish">Full Furnish</option>
-				</select>
-				</div>
+				<input type="text" class="form-control" name="fasilitas" value="<?php echo $rumah['fasilitas']?>">
+			</div>
 			</div>
 			<div class="form-group"> 
     					<div class="col-sm-offset-4 col-sm-4">
