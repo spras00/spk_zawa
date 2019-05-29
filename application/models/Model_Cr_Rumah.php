0765125785cec5e4e3b8ec6c19e7c7c629afafad
spras00 <50716392+spras00@users.noreply.github.com>
2019-05-29 17:00:26 +0700

    Update Model_Cr_Rumah.php

--- application/models/Model_Cr_Rumah.php
+++ application/models/Model_Cr_Rumah.php
@@ -21,7 +21,7 @@ Class Model_Cr_Rumah extends CI_Model{
                                 
         return $query->result();
     }
-	function baca_alt( $ID ){
+	function baca_alt( $id_r ){
 
 		 $query = $this->db->query("SELECT
             r.*, a.nm_a, k.nm_k
@@ -29,7 +29,7 @@ Class Model_Cr_Rumah extends CI_Model{
         	INNER JOIN _kriteria k ON k.id_k=r.id_k
             INNER JOIN _altrumah a ON a.id_a=r.id_a
             LEFT JOIN _crips c ON c.id_cp = r.id_cp
-        WHERE a.id_a='$ID' 
+        WHERE a.id_a='$id_r' 
         ORDER BY r.id_k");
                 
         return $query->result();
@@ -39,7 +39,7 @@ Class Model_Cr_Rumah extends CI_Model{
 
 	{
 		 foreach ($id_cp as $key => $val){                   
-            $this->db->update( 'r_altrumah', array('id_cp' =>$val), array('ID' => $key));   
+            $this->db->update( 'r_altrumah', array('id_cp' =>$val), array('id_r' => $key));   
         } 
 		
 	}
