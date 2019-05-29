ed19b175eff2542b73e58f629f13ccf6b339dadd
spras00 <50716392+spras00@users.noreply.github.com>
2019-05-29 16:53:39 +0700

    Update input_cr_rumah.php

--- application/views/input_cr_rumah.php
+++ application/views/input_cr_rumah.php
@@ -6,20 +6,23 @@
 ?>
 <?php echo form_open('cr_rumah/input/'.$rows[0]->id_a );?>
 <div class="form-horizontal">
-		 <?php foreach($rows as $row): ?>                    
+    <div class="col-md-offset-4 col-md-4">
+		 <?php foreach($rows as $row): ?>              
             <div class="form-group">
                 <label><?=$row->nm_k?> <span class="text-danger">*</span></label>
                 <select class="form-control" name="id_cp[<?=$row->ID?>]">
-                    <option></option>
-                    <?=get_crips_option( $row->id_k, set_value("id_cp[$row->ID]", $row->id_cp))?>
+                    <option>Pilih Nilai</option>
+                    <?=option_crips( $row->id_k, set_value("id_cp[$row->ID]", $row->id_cp))?>
+                </select>
                 </select>
             </div>
-            <?php endforeach?>		
+            <?php endforeach?>
+            </div>		
 			<div class="form-group"> 
     					<div class="col-sm-offset-4 col-sm-4">
       					<button type="submit" class="btn btn-success btn-block">Simpan</button>
       					<button type="reset" onclick="location.href='<?php echo base_url();?>cr_rumah'" class="btn btn-danger btn-block">Batal</button>
   						</div>
-  			</div>
-</div>
+
 <?php echo form_close();?>
+</div>
