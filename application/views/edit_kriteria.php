80ab5dd1b2649677c30fdad324b3b675ffea510c
spras00 <50716392+spras00@users.noreply.github.com>
2019-06-06 03:08:08 +0700

    Update edit_kriteria.php

--- application/views/edit_kriteria.php
+++ application/views/edit_kriteria.php
@@ -38,6 +38,7 @@
 			<div class="form-group"> 
     					<div class="col-sm-offset-4 col-sm-4">
       					<button type="submit" class="btn btn-success btn-block">Simpan</button>
+      					<button type="reset" onclick="location.href='<?php echo base_url();?>kriteria'" class="btn btn-danger btn-block">Batal</button>
   						</div>
   			</div>
 	</div>
