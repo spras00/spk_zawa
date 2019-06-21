c64c771b1c88e0f8684908d9c3170fd8a929a015
spras00 <50716392+spras00@users.noreply.github.com>
2019-06-22 02:46:49 +0700

    Update saw_zawa21.sql

--- saw_zawa21.sql
+++ saw_zawa21.sql
@@ -1,5 +1,5 @@
 ﻿# Host: 127.0.0.1  (Version 5.5.5-10.1.16-MariaDB)
-# Date: 2019-06-10 22:31:30
+# Date: 2019-06-22 00:17:20
 # Generator: MySQL-Front 6.1  (Build 1.26)
 
 
@@ -11,7 +11,7 @@ CREATE TABLE `_altrumah` (
   `id_a` varchar(5) NOT NULL DEFAULT '',
   `nm_a` varchar(75) NOT NULL DEFAULT '',
   `lokasi` varchar(100) NOT NULL DEFAULT '',
-  `harga` int(16) NOT NULL,
+  `harga` double(16,0) NOT NULL DEFAULT '0',
   `luas` int(3) NOT NULL DEFAULT '0',
   `tipe` varchar(15) NOT NULL DEFAULT '',
   `fasilitas` varchar(50) NOT NULL DEFAULT '',
@@ -22,7 +22,7 @@ CREATE TABLE `_altrumah` (
 # Data for table "_altrumah"
 #
 
-REPLACE INTO `_altrumah` VALUES ('A01','Rumah Telaga Kahuripan','Parung Bogor',650000000,180,'3','Kitchen Set, Closet Duduk, AC'),('A02','Cluster Perum Villa Pamulang','Tangerang',590000000,90,'2','Ac, Bathub, Kitchen Set, Pompa Air'),('A03','KS .Tubun Jakarta Barat','DKI Jakarta',1100000000,100,'3','Ac, Bathub, Kitchen Set, Pompa Air'),('A04','Vila Mutiara Cibubur','Depok',1600000000,120,'3','Kosong'),('A05','Puri Botanical Kebon Jeruk Jakarta Barat','DKI Jakarta',2147483647,70,'2','Kosong'),('A06','Komplek De Green Villa Mutiara Bandung','Bandung',1250000000,148,'2','AC, Kitchen Set'),('A07','Paramount Gading Serpong','Tangerang',2147483647,187,'3','Full Furnish'),('A08','Tebet Dalam ','DKI Jakarta',2147483647,157,'7','Ac, Shower, Kitchen Set, Pompa Air'),('A09','Tanjung Duren','DKI Jakarta',2100000000,55,'5','Ac, Shower, Kitchen Set, Pompa Air'),('A10','Bintaro','DKI Jakarta',2147483647,180,'5','Ac, Bathub, Kitchen Set, Pompa Air');
+REPLACE INTO `_altrumah` VALUES ('A01','Rumah Telaga Kahuripan','Parung Bogor',650000000,180,'3','Kitchen Set, Closet Duduk, AC'),('A02','Cluster Perum Villa Pamulang','Tangerang',590000000,90,'2','Ac, Bathub, Kitchen Set, Pompa Air'),('A03','KS .Tubun Jakarta Barat','DKI Jakarta',1100000000,100,'3','Ac, Bathub, Kitchen Set, Pompa Air'),('A04','Vila Mutiara Cibubur','Depok',1600000000,120,'3','Kosong'),('A05','Puri Botanical Kebon Jeruk Jakarta Barat','DKI Jakarta',2000000000,70,'2','Kosong'),('A06','Komplek De Green Villa Mutiara Bandung','Bandung',1250000000,148,'2','AC, Kitchen Set'),('A07','Paramount Gading Serpong','Tangerang',2950000000,187,'3','Full Furnish'),('A08','Tebet Dalam ','DKI Jakarta',2700000000,157,'7','Ac, Shower, Kitchen Set, Pompa Air'),('A09','Tanjung Duren','DKI Jakarta',2100000000,55,'5','Ac, Shower, Kitchen Set, Pompa Air'),('A10','Bintaro','DKI Jakarta',5500000000,180,'5','Ac, Bathub, Kitchen Set, Pompa Air');
 
 #
 # Structure for table "_kriteria"
@@ -60,7 +60,7 @@ CREATE TABLE `_crips` (
 # Data for table "_crips"
 #
 
-REPLACE INTO `_crips` VALUES ('CR11','C1','< 1 Milyar Rupiah',100),('CR12','C1','1 - 2 Milyar Rupiah',75),('CR13','C1','2 - 3 Milyar Rupiah',50),('CR14','C1','3 - 4 Milyar Rupiah',25),('CR15','C1','4 - 5 Milyar Rupiah',5),('CR21','C2','Jakarta',100),('CR22','C2','Botedabek',75),('CR23','C2','Luar Jabodetabek',50),('CR31','C3','>= 150 m2',100),('CR32','C3','<=100 <150',75),('CR33','C3','<50 <100',50),('CR34','C3','<=50',25),('CR41','C4','>= 4 Kamar Tidur',100),('CR42','C4','3 Kamar Tidur',75),('CR43','C4','2 Kamar Tidur',50),('CR44','C4','1 Kamar Tidur',25),('CR51','C5','Full Furnish',100),('CR52','C5','Semi Furnish',75),('CR53','C5','Un Furnish',50);
+REPLACE INTO `_crips` VALUES ('CR11','C1','< 1 Milyar Rupiah',100),('CR12','C1','1 - 2 Milyar Rupiah',75),('CR13','C1','2 - 3 Milyar Rupiah',50),('CR14','C1','3 - 4 Milyar Rupiah',25),('CR15','C1','4 - 5 Milyar Rupiah',5),('CR21','C2','Jakarta',100),('CR22','C2','Botedabek',75),('CR23','C2','Luar Jabodetabek',50),('CR31','C3','>= 150 m2',100),('CR32','C3','<=100 - 150 m2',75),('CR33','C3','<50 - 100 m2',50),('CR34','C3','<=50 m2',25),('CR41','C4','>= 4 Kamar Tidur',100),('CR42','C4','3 Kamar Tidur',75),('CR43','C4','2 Kamar Tidur',50),('CR44','C4','1 Kamar Tidur',25),('CR51','C5','Full Furnish',100),('CR52','C5','Semi Furnish',75),('CR53','C5','Un Furnish',50);
 
 #
 # Structure for table "_user"
@@ -87,7 +87,7 @@ CREATE TABLE `r_altrumah` (
   `id_k` varchar(5) NOT NULL,
   `id_cp` varchar(4) NOT NULL,
   PRIMARY KEY (`id_r`)
-) ENGINE=InnoDB AUTO_INCREMENT=109 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=116 DEFAULT CHARSET=latin1;
 
 #
 # Data for table "r_altrumah"
