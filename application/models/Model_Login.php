e4dba70562ecc0726b37b9b710a9ca748360ecff
spras00 <50716392+spras00@users.noreply.github.com>
2019-06-23 17:24:00 +0700

    Create Model_Login.php

---
+++ application/models/Model_Login.php
@@ -0,0 +1,19 @@
+<?php
+class Model_Login extends CI_Model {
+
+	function login($user, $pass){
+
+		$this->db->where('user', $user);
+		$this->db->where('pwd', $pass);
+		$query = $this->db->get('_user');
+
+		if($query->num_rows() > 0){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+
+
+}
