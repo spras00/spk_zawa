023fcc9122cf6ab41fe3ecf24adf3f7e7c190b50
spras00 <50716392+spras00@users.noreply.github.com>
2019-06-23 17:24:17 +0700

    Update routes.php

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,8 +49,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'Home';
-$route['kriteria'] = 'kriteria';
-$route['crips'] = 'crips';
+$route['default_controller'] = 'Login';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
