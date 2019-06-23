9e1695e6f49a015e0d51fb0ad8f7dbe2502122d0
spras00 <50716392+spras00@users.noreply.github.com>
2019-06-23 17:24:08 +0700

    Update Home.php

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -9,6 +9,10 @@ class Home extends CI_Controller
 	function __construct()
 	{
 		parent::__construct();
+		if (! $this->session->userdata('username'))
+        {
+            redirect('login');
+        }
 	}
 
 	function index(){
