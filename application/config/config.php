e2f245a64c788d98545b57165a587a9d91d809ea
spras00 <50716392+spras00@users.noreply.github.com>
2019-06-23 17:24:19 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/spk_zawa';
+$config['base_url'] = 'http://localhost/spk_zawa/';
 
 /*
 |--------------------------------------------------------------------------
