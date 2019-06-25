316e0476e064d60b0139958567764cd7df7a32da
spras00 <50716392+spras00@users.noreply.github.com>
2019-06-26 03:06:32 +0700

    Update login.php

--- application/views/login.php
+++ application/views/login.php
@@ -27,7 +27,7 @@
     <div class="row">
         <div class="col-md-4 col-md-offset-4">
             <div class="panel panel-default">
-                <div class="panel-heading text-center">Silahkan Login</div>
+                <div class="panel-heading text-center"><b>Login SPK</b> <img src="ast/c21_icon.jpg"></div>
                 <div class="panel-body">
                     <?=print_error()?>
                     <?php echo form_open('login');?>
