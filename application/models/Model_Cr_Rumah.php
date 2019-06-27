933d86497ede348834fa3608560de92e839e7e32
spras00 <50716392+spras00@users.noreply.github.com>
2019-06-28 01:47:12 +0700

    Update Model_Cr_Rumah.php

--- application/models/Model_Cr_Rumah.php
+++ application/models/Model_Cr_Rumah.php
@@ -4,30 +4,36 @@
 Class Model_Cr_Rumah extends CI_Model{
 	
 	function __construct()
+    
 	{
 		parent::__construct();
 	}
 
 
-	function tampil_data(){
+	function tampil_data()
+
+    {
 
 		$query = $this->db->query("SELECT r.*, a.nm_a, c.nm_cp
         FROM r_altrumah r
-            INNER JOIN _kriteria k ON k.id_k=r.id_k
-            INNER JOIN _altrumah a ON a.id_a=r.id_a
-            LEFT JOIN _crips c ON c.id_cp = r.id_cp
+            INNER JOIN kriteria k ON k.id_k=r.id_k
+            INNER JOIN altrumah a ON a.id_a=r.id_a
+            LEFT JOIN crips c ON c.id_cp = r.id_cp
         ORDER BY r.id_a, r.id_k");
                                 
         return $query->result();
     }
-	function baca_alt( $id_r ){
+
+	function baca_alt( $id_r )
+
+    {
 
 		 $query = $this->db->query("SELECT
             r.*, a.nm_a, k.nm_k
         FROM r_altrumah r 
-        	INNER JOIN _kriteria k ON k.id_k=r.id_k
-            INNER JOIN _altrumah a ON a.id_a=r.id_a
-            LEFT JOIN _crips c ON c.id_cp = r.id_cp
+        	INNER JOIN kriteria k ON k.id_k=r.id_k
+            INNER JOIN altrumah a ON a.id_a=r.id_a
+            LEFT JOIN crips c ON c.id_cp = r.id_cp
         WHERE a.id_a='$id_r' 
         ORDER BY r.id_k");
                 
