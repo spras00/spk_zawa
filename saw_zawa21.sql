4bdee237a8c3807c0ed77b8a7374f159e2b81fd5
spras00 <50716392+spras00@users.noreply.github.com>
2019-07-01 02:04:24 +0700

    Update saw_zawa21.sql

--- saw_zawa21.sql
+++ saw_zawa21.sql
@@ -1,5 +1,5 @@
 ﻿# Host: 127.0.0.1  (Version 5.5.5-10.1.16-MariaDB)
-# Date: 2019-06-28 01:46:41
+# Date: 2019-07-01 00:16:19
 # Generator: MySQL-Front 6.1  (Build 1.26)
 
 
@@ -25,6 +25,24 @@ CREATE TABLE `altrumah` (
 REPLACE INTO `altrumah` VALUES ('A01','Rumah Telaga Kahuripan','Parung Bogor',650000000,180,'3','Kitchen Set, Closet Duduk, AC'),('A02','Cluster Perum Villa Pamulang','Tangerang',590000000,90,'2','Ac, Bathub, Kitchen Set, Pompa Air'),('A03','KS .Tubun Jakarta Barat','DKI Jakarta',1100000000,100,'3','Ac, Bathub, Kitchen Set, Pompa Air'),('A04','Vila Mutiara Cibubur','Depok',1600000000,120,'3','Kosong'),('A05','Puri Botanical Kebon Jeruk Jakarta Barat','DKI Jakarta',2000000000,70,'2','Kosong'),('A06','Komplek De Green Villa Mutiara Bandung','Bandung',1250000000,148,'2','AC, Kitchen Set'),('A07','Paramount Gading Serpong','Tangerang',2950000000,187,'3','Full Furnish'),('A08','Tebet Dalam ','DKI Jakarta',2700000000,157,'7','Ac, Shower, Kitchen Set, Pompa Air'),('A09','Tanjung Duren','DKI Jakarta',2100000000,55,'5','Ac, Shower, Kitchen Set, Pompa Air'),('A10','Bukit Serpong Mas','Tangerang Selatan',1500000000,120,'2','2 AC, 2 tempat tidur, kitcen set, kulkas, 1 set sofa, 1 set meja makan, kompor gas');
 
 #
+# Structure for table "crips"
+#
+
+CREATE TABLE `crips` (
+  `id_cp` varchar(4) NOT NULL,
+  `id_k` varchar(5) NOT NULL,
+  `nm_cp` varchar(50) NOT NULL DEFAULT '',
+  `skor` double NOT NULL,
+  PRIMARY KEY (`id_cp`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+#
+# Data for table "crips"
+#
+
+REPLACE INTO `crips` VALUES ('CR11','C1','<= 1 Milyar Rupiah',100),('CR12','C1','1 - <= 2 Milyar Rupiah',75),('CR13','C1','2 - <= 3 Milyar Rupiah',50),('CR14','C1','3 - <= 4 Milyar Rupiah',25),('CR21','C2','Jakarta',100),('CR22','C2','Botedabek',75),('CR23','C2','Luar Jabodetabek',50),('CR31','C3','>= 150 m2',100),('CR32','C3','<=100 - 150 m2',75),('CR33','C3','<50 - 100 m2',50),('CR34','C3','<=50 m2',25),('CR41','C4','>= 4 Kamar Tidur',100),('CR42','C4','3 Kamar Tidur',75),('CR43','C4','2 Kamar Tidur',50),('CR44','C4','1 Kamar Tidur',25),('CR51','C5','Full Furnish',100),('CR52','C5','Semi Furnish',75),('CR53','C5','Un Furnish',50);
+
+#
 # Structure for table "kriteria"
 #
 
@@ -43,26 +61,6 @@ CREATE TABLE `kriteria` (
 REPLACE INTO `kriteria` VALUES ('C1','HARGA','0',30),('C2','LOKASI','1',20),('C3','LUAS','1',20),('C4','TIPE','1',15),('C5','FASILITAS','1',15);
 
 #
-# Structure for table "crips"
-#
-
-CREATE TABLE `crips` (
-  `id_cp` varchar(4) NOT NULL,
-  `id_k` varchar(5) NOT NULL,
-  `nm_cp` varchar(50) NOT NULL DEFAULT '',
-  `skor` double NOT NULL,
-  PRIMARY KEY (`id_cp`),
-  KEY `id_k` (`id_k`),
-  CONSTRAINT `crips_ibfk_1` FOREIGN KEY (`id_k`) REFERENCES `kriteria` (`id_k`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-#
-# Data for table "crips"
-#
-
-REPLACE INTO `crips` VALUES ('CR11','C1','<= 1 Milyar Rupiah',100),('CR12','C1','1 - <= 2 Milyar Rupiah',75),('CR13','C1','2 - <= 3 Milyar Rupiah',50),('CR14','C1','3 - <= 4 Milyar Rupiah',25),('CR21','C2','Jakarta',100),('CR22','C2','Botedabek',75),('CR23','C2','Luar Jabodetabek',50),('CR31','C3','>= 150 m2',100),('CR32','C3','<=100 - 150 m2',75),('CR33','C3','<50 - 100 m2',50),('CR34','C3','<=50 m2',25),('CR41','C4','>= 4 Kamar Tidur',100),('CR42','C4','3 Kamar Tidur',75),('CR43','C4','2 Kamar Tidur',50),('CR44','C4','1 Kamar Tidur',25),('CR51','C5','Full Furnish',100),('CR52','C5','Semi Furnish',75),('CR53','C5','Un Furnish',50);
-
-#
 # Structure for table "r_altrumah"
 #
 
@@ -71,11 +69,7 @@ CREATE TABLE `r_altrumah` (
   `id_a` varchar(5) NOT NULL DEFAULT '0',
   `id_k` varchar(5) NOT NULL,
   `id_cp` varchar(4) NOT NULL,
-  PRIMARY KEY (`id_r`),
-  KEY `id_a` (`id_a`),
-  KEY `id_k` (`id_k`),
-  CONSTRAINT `r_altrumah_ibfk_1` FOREIGN KEY (`id_a`) REFERENCES `altrumah` (`id_a`),
-  CONSTRAINT `r_altrumah_ibfk_2` FOREIGN KEY (`id_k`) REFERENCES `kriteria` (`id_k`)
+  PRIMARY KEY (`id_r`)
 ) ENGINE=InnoDB AUTO_INCREMENT=136 DEFAULT CHARSET=latin1;
 
 #
