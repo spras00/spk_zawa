b4c6664c59c438cbc46244c9a0955611135ae9fd
spras00 <50716392+spras00@users.noreply.github.com>
2019-07-07 02:01:32 +0700

    Update view_cr_rumah.php

--- application/views/view_cr_rumah.php
+++ application/views/view_cr_rumah.php
@@ -1,23 +1,29 @@
 </div>	
   <?php           
-            $relasi = array(); 
+            $relasi = array();
+            if(empty($relasi)){echo "" ;}
             foreach ($rows as $row) {                
                 $alternatif[$row->id_a] = $row->nm_a;
                 $relasi[$row->id_a][$row->id_k] = $row->nm_cp;                
-            }                                
+        }             
         ?>
 <table align="center" class="table table-hover">
 	  <thead><tr>
                 <th>ID Rumah</th>
                 <th>Nama Rumah</th>
-                <?php 
+                <?php
                 $first = array_values($relasi);
+                if(empty($first)){echo "" ;}
+                else
                 foreach ($first[0] as $key => $val):?>
                     <th><?=$key?></th>
-                <?php endforeach ?>
+                <?php endforeach?>
                 <th></th>
             </tr></thead>    
-            <?php foreach ($alternatif as $key => $value):?>
+            <?php 
+                if(empty($first)){echo "" ;}
+                else
+            foreach ($alternatif as $key => $value):?>
             <tr>
                 <td><?=$key?></td>
                 <td><?=$value?></td>
@@ -29,5 +35,32 @@
                 </td>
             </tr>
             <?php endforeach?>
+<div class="btn header-btn pull-right">
+<button type="button" id="next" class="btn btn-default" onclick="c_null()">
+Perhitungan SAW
+</button>
+</div>
 </table>
 </div>
+<script>
+    function c_null() {
+    var a = <?php echo json_encode($relasi); ?>;
+    var go = "<?php echo site_url('hitung'); ?>";
+
+        for(var i in a){
+            if(a.hasOwnProperty(i)){
+                for(var j in a[i]){
+                    if(a[i].hasOwnProperty(j)){
+                        if (a[i][j] == null) {
+                            alert("Nilai "+(j)+" pada "+(i)+" tidak boleh kosong.");
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            if (a[i][j] != null){
+                                if (confirm("Lanjutkan ke perhitungan?"))
+                                window.location.href = go;
+                            }
+}
+</script>
