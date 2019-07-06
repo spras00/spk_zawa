af9f9d53c09178089c88104b08316995260eb74e
spras00 <50716392+spras00@users.noreply.github.com>
2019-07-07 02:01:49 +0700

    Update Model_Kriteria.php

--- application/models/Model_Kriteria.php
+++ application/models/Model_Kriteria.php
@@ -59,4 +59,31 @@ class Model_Kriteria extends CI_Model{
 		$this->db->delete('r_altrumah', array('id_k' => $id_k));
 		$this->db->delete('kriteria', array('id_k' => $id_k));
 	}
+
+	function c_bobot($str)
+	{	
+		$data = $this->db->select_sum('bobot')->get('kriteria');
+		$total = $data->row_array();
+		$total['bobot'] += $str;
+		$sum = $total['bobot'];
+		if ($sum > 100) {
+			return FALSE;
+		}else if ($sum == 0) {
+			return TRUE;
+		}else
+			return TRUE;
+	}
+
+	function c_bobot_e($str, $x)
+	{	
+		$data = $this->db->select_sum('bobot')->where_not_in('kriteria.id_k', $x)->get('kriteria');
+		$total = $data->row_array();
+		$total['bobot'] += $str;
+		$sum = $total['bobot'];
+		if ($sum > 100) {
+			return FALSE;
+		}else
+			return TRUE;
+		}
+
 }
